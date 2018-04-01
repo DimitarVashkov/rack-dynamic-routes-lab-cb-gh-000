0c6a251c92dded9dea1e0e2e240783541c731df3
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-01 17:16:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/songs/)
+    if req.path.match(/items/)
 
-      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
+      song_title = req.path.split("/item/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s. == song_title}
 
       resp.write song.artist
     else
